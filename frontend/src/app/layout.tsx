6b9ca0e61090275bd912e9f37bc94a2e531fa6f7
Liandro Chaves <liandrochaves18@gmail.com>
2025-06-09 10:48:35 -0300

    icone do site e faltando editar bandeirtas

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,18 +2,30 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "../utils/context/ThemeContext";
 import { LoadingProvider } from "../utils/context/LoadingProvider";
-import LayoutContent from "../utils/utilities/LayoutContent"; // import novo
+import LayoutContent from "../utils/utilities/LayoutContent";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
     title: "Cadastro de Atletas",
     description: "Projeto de gestão de atletas",
+    icons: [
+        { rel: "apple-touch-icon", url: "/apple-icon.png", sizes: "180x180" },
+        { rel: "icon", url: "/favicon.ico" },
+        { rel: "manifest", url: "/manifest.json" },
+    ],
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="pt-BR" suppressHydrationWarning>
+            <Head>
+                {/* Meta tags extras não suportadas no metadata */}
+                <meta name="msapplication-TileColor" content="#ffffff" />
+                <meta name="msapplication-config" content="/browserconfig.xml" />
+                <meta name="theme-color" content="#ffffff" />
+            </Head>
             <body className={inter.className}>
                 <ThemeProvider>
                     <LoadingProvider>
